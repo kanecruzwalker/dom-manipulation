bbad5c3c3e5e8c29d3a71f06ff2a85115ef6f876
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-07-20 19:20:32 -0700

    styled background color

--- script.js
+++ script.js
@@ -5,17 +5,34 @@ var input = document.querySelector("#input1");
 var add = document.querySelector("#add");
 
 add.addEventListener("click", addContentClick);
+input.addEventListener("keypress", addContentEnter);
+
 
 function addContentClick(){
     console.log(event);
     console.log(input.value);
+    addItem();
 }
-
-input.addEventListener("keypress", addContentEnter);
 
 function addContentEnter(){
     if (event.keyCode === 13){
         console.log(event);
         console.log(input.value);
+        addItem();
     }
 }
+
+function addItem(){
+    var list = document.querySelector("#list");
+    console.log(list);
+    list.append("text");
+}
+
+
+// Select Input Field
+
+// Select the Submit button
+
+// Add enent listener for click
+// Add event listener for keydown
+// Put input at the bottom of the list 
